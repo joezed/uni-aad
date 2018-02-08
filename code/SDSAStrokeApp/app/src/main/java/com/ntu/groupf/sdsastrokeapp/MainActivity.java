7d1a80152e005c5709e7cf5609174c4b6c85fa1c
SamLCrane <SamuelLCrane@gmail.com>
2018-02-08 20:53:06 +0000

    Log in interface final
    Should all be cleared up now

--- code/SDSAStrokeApp/app/src/main/java/com/ntu/groupf/sdsastrokeapp/MainActivity.java
+++ code/SDSAStrokeApp/app/src/main/java/com/ntu/groupf/sdsastrokeapp/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //TextView tv = (TextView) findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
     }
 
     /**
