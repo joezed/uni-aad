4879a5b96122f0e748aca70f8799ce6d824ee81c
SamLCrane <SamuelLCrane@gmail.com>
2018-02-15 02:16:30 +0000

    rsr complete

--- code/SDSAStrokeApp/app/src/main/java/com/ntu/groupf/sdsastrokeapp/RoadSignRecognitionActivity.java
+++ code/SDSAStrokeApp/app/src/main/java/com/ntu/groupf/sdsastrokeapp/RoadSignRecognitionActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import java.util.HashMap;
@@ -15,6 +16,7 @@ public class RoadSignRecognitionActivity extends AppCompatActivity implements Vi
     private ViewFlipper vf_scenes;
     private Button btn_next;
     private Button btn_prev;
+    private Button btn_submit;
     private ImageView img_selected;
     private ImageView img_sign1;
     private ImageView img_sign2;
@@ -33,7 +35,8 @@ public class RoadSignRecognitionActivity extends AppCompatActivity implements Vi
 
     private Map<Integer, Integer> answers = new HashMap<>();
     private int sceneID = 1;
-    private int signID;
+    private int signID = 0;
+    private int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +51,7 @@ public class RoadSignRecognitionActivity extends AppCompatActivity implements Vi
         vf_scenes = (ViewFlipper) findViewById(R.id.vf_scenes);
         btn_next = (Button) findViewById(R.id.btn_next);
         btn_prev = (Button) findViewById(R.id.btn_prev);
+        btn_submit = (Button) findViewById(R.id.btn_submit);
         img_selected = (ImageView) findViewById(R.id.img_selected);
         img_sign1 = (ImageView) findViewById(R.id.img_sign1);
         img_sign2 = (ImageView) findViewById(R.id.img_sign2);
@@ -66,6 +70,7 @@ public class RoadSignRecognitionActivity extends AppCompatActivity implements Vi
 
         btn_next.setOnClickListener(this);
         btn_prev.setOnClickListener(this);
+        btn_submit.setOnClickListener(this);
         img_selected.setOnClickListener(this);
         img_sign1.setOnClickListener(this);
         img_sign2.setOnClickListener(this);
@@ -94,6 +99,7 @@ public class RoadSignRecognitionActivity extends AppCompatActivity implements Vi
             if (sceneID > 12) {
                 sceneID = 1;
             }
+            putAnswer(answers.get(sceneID));
         } else if(view == btn_prev) {
             vf_scenes.showPrevious();
             answers.put(sceneID, signID);
@@ -101,67 +107,113 @@ public class RoadSignRecognitionActivity extends AppCompatActivity implements Vi
             if (sceneID < 1) {
                 sceneID = 12;
             }
-        } else {
-            setPreview(view);
+            putAnswer(answers.get(sceneID));
+        } else if (view == btn_submit) {
+            answers.put(sceneID, signID);
+            score = 0;
+            for (int i = 1; i <= answers.size(); i++) {
+                if (i == answers.get(i)) {
+                    score += 1;
+                }
+            }
+            Toast.makeText(RoadSignRecognitionActivity.this, "Score: " + Integer.toString(score),
+                    Toast.LENGTH_SHORT).show();
+        }  else {
+            signClick(view);
         }
     }
 
-    public void setPreview(View view) {
+    public void signClick(View view) {
         if (view == img_sign1) {
             img_selected.setImageResource(R.drawable.sign1);
-            sceneID = 1;
             signID = 1;
         } else if (view == img_sign2) {
             img_selected.setImageResource(R.drawable.sign2);
-            sceneID = 2;
             signID = 2;
         } else if (view == img_sign3) {
             img_selected.setImageResource(R.drawable.sign3);
-            sceneID = 3;
             signID = 3;
         } else if (view == img_sign4) {
             img_selected.setImageResource(R.drawable.sign4);
-            sceneID = 4;
             signID = 4;
         } else if (view == img_sign5) {
             img_selected.setImageResource(R.drawable.sign5);
-            sceneID = 5;
             signID = 5;
         } else if (view == img_sign6) {
             img_selected.setImageResource(R.drawable.sign6);
-            sceneID = 6;
             signID = 6;
         } else if (view == img_sign7) {
             img_selected.setImageResource(R.drawable.sign7);
-            sceneID = 7;
             signID = 7;
         } else if (view == img_sign8) {
             img_selected.setImageResource(R.drawable.sign8);
-            sceneID = 8;
             signID = 8;
         } else if (view == img_sign9) {
             img_selected.setImageResource(R.drawable.sign9);
-            sceneID = 9;
             signID = 9;
         } else if (view == img_sign10) {
             img_selected.setImageResource(R.drawable.sign10);
-            sceneID = 10;
             signID = 10;
         } else if (view == img_sign11) {
             img_selected.setImageResource(R.drawable.sign11);
-            sceneID = 11;
             signID = 11;
         } else if (view == img_sign12) {
             img_selected.setImageResource(R.drawable.sign12);
-            sceneID = 12;
             signID = 12;
         } else if (view == img_sign13) {
             img_selected.setImageResource(R.drawable.sign13);
-            sceneID = 13;
             signID = 13;
         } else if (view == img_sign14) {
             img_selected.setImageResource(R.drawable.sign14);
-            sceneID = 14;
+            signID = 14;
+        }
+    }
+
+    public void putAnswer(int next) {
+        if (next == 0) {
+            img_selected.setImageResource(R.drawable.sign_blank);
+            signID = 0;
+        } else if (next == 1) {
+            img_selected.setImageResource(R.drawable.sign1);
+            signID = 1;
+        } else if (next == 2) {
+            img_selected.setImageResource(R.drawable.sign2);
+            signID = 2;
+        } else if (next == 3) {
+            img_selected.setImageResource(R.drawable.sign3);
+            signID = 3;
+        } else if (next == 4) {
+            img_selected.setImageResource(R.drawable.sign4);
+            signID = 4;
+        } else if (next == 5) {
+            img_selected.setImageResource(R.drawable.sign5);
+            signID = 5;
+        } else if (next == 6) {
+            img_selected.setImageResource(R.drawable.sign6);
+            signID = 6;
+        } else if (next == 7) {
+            img_selected.setImageResource(R.drawable.sign7);
+            signID = 7;
+        } else if (next == 8) {
+            img_selected.setImageResource(R.drawable.sign8);
+            signID = 8;
+        } else if (next == 9) {
+            img_selected.setImageResource(R.drawable.sign9);
+            signID = 9;
+        } else if (next == 10) {
+            img_selected.setImageResource(R.drawable.sign10);
+            signID = 10;
+        } else if (next == 11) {
+            img_selected.setImageResource(R.drawable.sign11);
+            signID = 11;
+        } else if (next == 12) {
+            img_selected.setImageResource(R.drawable.sign12);
+            signID = 12;
+        } else if (next == 13) {
+            img_selected.setImageResource(R.drawable.sign13);
+            signID = 13;
+        } else if (next == 14) {
+            img_selected.setImageResource(R.drawable.sign14);
             signID = 14;
         }
     }
